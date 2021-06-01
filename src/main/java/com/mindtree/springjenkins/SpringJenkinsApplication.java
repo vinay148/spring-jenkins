e8b3ee1ecff20fbf6e5c17132af1d55f3b346324
vinay148 <vk720213@gmail.com>
2021-06-01 16:51:55 +0530

    second commit

--- src/main/java/com/mindtree/springjenkins/SpringJenkinsApplication.java
+++ src/main/java/com/mindtree/springjenkins/SpringJenkinsApplication.java
@@ -1,13 +1,35 @@
 package com.mindtree.springjenkins;
 
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+  
+
 @SpringBootApplication
 public class SpringJenkinsApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	
+	@PostConstruct
+	public void init() {
+		logger.info("application started");
+	}
+	
 
 	public static void main(String[] args) {
+		logger.info("application executed...");
+		logger.info("application executed second...");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
 }
+
+
+
