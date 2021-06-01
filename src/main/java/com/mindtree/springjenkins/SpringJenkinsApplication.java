701498e54e459b2051f0f98fd9f29a1f089aedad
vinay148 <vk720213@gmail.com>
2021-06-01 17:23:39 +0530

    fourrr commit

--- src/main/java/com/mindtree/springjenkins/SpringJenkinsApplication.java
+++ src/main/java/com/mindtree/springjenkins/SpringJenkinsApplication.java
@@ -24,7 +24,7 @@ public class SpringJenkinsApplication {
 	
 
 	public static void main(String[] args) {
-		logger.info("application executedddddd...");
+		logger.info("application executedddddddd...");
 		logger.info("application executed second...");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
